ac97432db6bb15c3725a8ae9fa6f5869d43e96a1
Doug Issichopoulos <41125998+dougissi@users.noreply.github.com>
2022-01-25 07:36:25 -0800

    wordle keyboard key button class rename

--- assets/wordle_helper/solver.js
+++ assets/wordle_helper/solver.js
@@ -516,7 +516,7 @@ $(document).on("keydown", function(event) {
 })
 
 // when keyboard button is clicked
-$(".xxx").click( function() {
+$(".key-button").click( function() {
   let key = $(this).text();
   let keyCode = this.id;
   keyActions(key, keyCode);
