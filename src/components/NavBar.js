68d9d582d0c5296d097c2e34a6a55163dc9e4394
Doug Issichopoulos <41125998+dougissi@users.noreply.github.com>
2024-06-15 13:39:51 -0700

    simplify NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,29 @@
-import { useState } from 'react';
 import { Link as RouterLink} from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { Stack } from '@mui/material';
+import { Link } from '@mui/material';
 
 
-const pages = ['Products', 'Pricing', 'Blog'];
 const siteTitle = 'DOUGISSI';
 
 export default function NavBar() {
-  const [anchorElNav, setAnchorElNav] = useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
+  const NavIconButton = ({ href, icon }) => {
+    return (
+      <IconButton
+        component={Link}
+        href={href}
+        sx={{ color: 'inherit' }}
+      >
+        {icon}
+      </IconButton>
+    );
   };
 
   return (
@@ -36,8 +36,6 @@ export default function NavBar() {
             component={RouterLink}
             to="/"
             sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
               fontFamily: 'monospace',
               fontWeight: 700,
               letterSpacing: '.3rem',
@@ -48,72 +46,19 @@ export default function NavBar() {
             {siteTitle}
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
-          <Typography
-            variant="h5"
-            noWrap
-            component={RouterLink}
-            to="/"
+          <Stack
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
+            spacing={0}
             sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
               flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              display: 'flex'
             }}
           >
-            {siteTitle}
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+            <NavIconButton href="https://github.com/dougissi" icon={<GitHubIcon />} />
+            <NavIconButton href="https://www.linkedin.com/in/dougissi/" icon={<LinkedInIcon />} />
+          </Stack>
 
         </Toolbar>
       </Container>
