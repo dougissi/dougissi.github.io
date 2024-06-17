0532b0f3b888dc568eee3604fa17154c08e8f766
Doug Issichopoulos <41125998+dougissi@users.noreply.github.com>
2024-06-17 08:41:36 -0700

    add post summaries and category/lang tags as row of Chips to posts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
+import dayjs from 'dayjs';
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import BlogPage from './components/BlogPage';
+import { Chip, Stack } from '@mui/material';
 
 let posts = [
   {
     id: 13,
     title: 'WordleReplay.com Overhaul with React and Material UI',
-    summary: '',
+    summary: 'Complete rewrite of WordleReplay.com web app using React and Material UI, greatly improving the user experience compared to the original from 2022 that used only JQuery and Bootstrap',
     mdFileName: '2024-06-10-wordlereplay-react.md',
     imgFileName: '/assets/wordlereplay/wordlereplay_react_mui.png',
     categories: ['Web Development'],
@@ -17,7 +19,7 @@ let posts = [
   {
     id: 12,
     title: 'HaleyIssi.com: React App on GitHub Pages with Google Sheets Database',
-    summary: '',
+    summary: 'My wife Haley has been growing some of her Instagram accounts around some her hobbies like latte art and cooking, so we desired to create a simple website where we could post referral links for products on Amazon',
     mdFileName: '2024-04-18-haleyissi.md',
     // imgFileName: '/assets/images/haleyissi-logo.png',
     imgFileName: 'https://haleyissi.com/static/media/haleyissi-coffee-portrait-440kb.cd3af01ed82140f2b5df.jpg',
@@ -27,7 +29,7 @@ let posts = [
   {
     id: 11,
     title: 'Hybrid Tensor Sharing Between Serverless Functions',
-    summary: '',
+    summary: "Capstone research paper at the end of my CS master's program where we discovered a faster way to share Deep Neural Network tensors between AWS Lambda functions",
     mdFileName: '2023-12-13-hybrid-tensor-sharing.md',
     imgFileName: '/assets/images/tensor_sharing_rest_vs_s3.png',
     categories: ['Machine Learning'],
@@ -36,7 +38,7 @@ let posts = [
   {
     id: 10,
     title: 'Stateful Graph Algorithms in Haskell',
-    summary: '',
+    summary: 'Seeing as Haskell is a functional programming language, managing state requires great care, so I explored this concept by implementing several graph algorithms',
     mdFileName: '2023-08-01-stateful-graph-algorithms-haskell.md',
     imgFileName: '/assets/graph_algos_haskell/haskell_graph_logo.png',
     categories: ['Graphs', 'Algorithms'],
@@ -45,6 +47,7 @@ let posts = [
   {
     id: 9,
     title: 'Parking Spot Detection Prototype with 3 Cameras',
+    summary: "When approaching a large, busy parking lot, wouldn't it be nice to be able to navigate directly to an open parking spot? Well, I made a prototype demonstrating that it's possible.",
     mdFileName: '2023-05-07-parking-spot-detection.md',
     imgFileName: '/assets/images/parking_spot_detection_prototype.jpg',
     categories: ['Computer Vision', 'Internet of Things'],
@@ -53,7 +56,7 @@ let posts = [
   {
     id: 8,
     title: 'Self-Driving Vehicle using a Raspberry Pi',
-    summary: '',
+    summary: 'With this small programmable car equipped with an ultrasonic sensor and a camera, and I was able to implement simple yet powerful self-driving capabilities',
     mdFileName: '2023-02-05-picar-full-self-driving.md',
     imgFileName: '/assets/images/picar.png',
     categories: ['Internet of Things'],
@@ -62,7 +65,7 @@ let posts = [
   {
     id: 7,
     title: 'Square Root Implementation',
-    summary: '',
+    summary: "During the final stage of a three-part technical interview for Senior Software Engineer position at a publicly traded software company in Silicon Valley, this question got me, which was humiliating given my BS in Mathematics from Stanford. Don't neglect learning CS, kids.",
     mdFileName: '2022-11-26-square-root.md',
     imgFileName: '/assets/square_root/sqr_root_logo.png',
     categories: ['Math', 'Algorithms'],
@@ -71,7 +74,7 @@ let posts = [
   {
     id: 6,
     title: 'Is It Bad to Move to a Higher Tax Bracket?',
-    summary: '',
+    summary: "Spoiler: it's not bad to move to a higher tax bracket. Even though I had been doing my taxes by hand since 2018, it wasn't until I performed this analysis that I finally understood why.",
     mdFileName: '2022-11-13-tax-brackets.md',
     imgFileName: '/assets/tax_brackets/next-tax-bracket_2400x1258.jpg',
     categories: ['Finance', 'Visualization'],
@@ -80,6 +83,7 @@ let posts = [
   {
     id: 5,
     title: 'Wordle Replay',
+    summary: "By early 2022, Wordle had taken the world by storm, so as my first genuine attempt at web development, I built WordleReplay.com to both help solve puzzles and allow users to replay old ones.",
     mdFileName: '2022-01-31-wordle-replay.md',
     imgFileName: '/assets/wordlereplay/wordle_replay_share_icon_1200x600.png',
     categories: ['Web Development'],
@@ -89,7 +93,7 @@ let posts = [
   {
     id: 4,
     title: "Flipping Cards 'Til You Can't",
-    summary: '',
+    summary: "Interactive simulation of a card-flipping game from a viral movie clip — a game that, mathematically, must end",
     mdFileName: '2021-12-04-x-y-card-flipping.md',
     imgFileName: '/assets/x_plus_y_demo/assets/flipping_cards_icon.png',
     categories: ['Web Development', 'Algorithms', 'Math'],
@@ -98,7 +102,7 @@ let posts = [
   {
     id: 3,
     title: 'Counting Polygons in Node Graphs',
-    summary: '',
+    summary: "After learning how to count triangles in node graphs (see previous post), I developed new algorithms to count quadrilaterals and pentagons in node graphs.",
     mdFileName: '2020-02-10-counting-polygons-in-node-graphs.md',
     imgFileName: 'https://www.dougissi.com/counting-polygons/assets/counting_polygons_icon.jpg',
     categories: ['Graphs', 'Algorithms'],
@@ -107,7 +111,7 @@ let posts = [
   {
     id: 2,
     title: 'Counting Triangles in Node Graphs',
-    summary: '',
+    summary: "At work I came across an elegant method to count the number of triangles in a node graph, so I set out to understand why it works.",
     mdFileName: '2020-02-06-counting-triangles-in-node-graphs.md',
     imgFileName: 'https://www.dougissi.com/counting-triangles/assets/counting_triangles_icon.jpg',
     categories: ['Graphs', 'Algorithms'],
@@ -116,7 +120,7 @@ let posts = [
   {
     id: 1,
     title: 'Export Apple Contacts to CSV',
-    summary: '',
+    summary: "Wouldn't it be really great if you could get all the emails, phone numbers, and addresses, of the people your Apple Contacts into a spreadsheet?",
     mdFileName: '2019-12-07-export-apple-contacts-to-csv.md',
     imgFileName: '/assets/images/apple_contacts_football.jpg',
     categories: ['Tools'],
@@ -128,7 +132,28 @@ let posts = [
 posts = posts.map((post) => {
   const date = post.mdFileName.slice(0, 10);
   const path = post.mdFileName.slice(11, post.mdFileName.length - 3);
-  return {...post, date: date, path: path};
+  return {
+    ...post,
+    date: dayjs(date).format('MMMM D, YYYY'),
+    path: path,
+    tags: (
+      <Stack
+        direction="row"
+        spacing={1}
+        justifyContent="center"
+        alignItems="center"
+        useFlexGap
+        flexWrap="wrap"
+      >
+        {post.categories.map(cat => (
+          <Chip key={`chip-cat-${cat}-post${post.id}`} label={cat} color="primary" variant="outlined" />
+        ))}
+        {post.languages.map(lang => (
+          <Chip key={`chip-lang-${lang}-post${post.id}`} label={lang} color="success" variant="outlined" />
+        ))}
+      </Stack>
+    )
+  };
 });
 
 // get unique categories and languages
