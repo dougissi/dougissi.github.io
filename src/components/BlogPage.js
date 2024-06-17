2b6bb914b6005f2dfac43ba1e495f1163a39e033
Doug Issichopoulos <41125998+dougissi@users.noreply.github.com>
2024-06-17 08:57:47 -0700

    set Markdown and BlogCards max-width; otherwise can get too wide on desktop

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -4,7 +4,16 @@ import Markdown from './Markdown';
 export default function BlogPage({ post }) {
   return (
     <div className="BlogPage" style={{ paddingTop: '10px' }}>
-      <Typography variant="h4" component="div">
+      <Typography
+        variant="h4"
+        component="div"
+        style={{  // TODO: commonize with .Markdown class and BlogCard
+          margin: 'auto',
+          maxWidth: '1000px',
+          paddingRight: '5%',
+          paddingLeft: '5%'
+        }}
+      >
         {post.title}
       </Typography>
       <Typography gutterBottom variant="h5" component="div" color="text.secondary">
